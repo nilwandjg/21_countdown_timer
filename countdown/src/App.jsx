f210e799bb6e069e601f4dbab83491249823e943
nilwandjg <nilwandjg@yahoo.com.br>
2023-01-20 02:13:04 -0300

    Criação dos componentes Title e Counter, mais modificação no App.jsx

--- countdown/src/App.jsx
+++ countdown/src/App.jsx
@@ -1,10 +1,22 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+
+import NewYear from "./assets/newyear.jpg"
 import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
-      <h1>Countdown</h1>
+    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+      <div className="container">
+        <Title title="Contagem regressiva para 2024" />
+        <div className="countdown-container">
+          <Counter title="Dias" number={2} />
+          <Counter title="Horas" number={2} />
+          <Counter title="Minutos" number={2} />
+          <Counter title="Segundos" number={2} />
+        </div>
+      </div>
     </div>
   )
 }
